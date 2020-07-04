69273bf8457f75de8f38f3f7edc9b7b38a7583fe
chon <chonhnm@gmail.com>
2020-07-04 15:17:03 +0800

    exe 1.1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const Hello = (props) => (
-  <div>
-    <p>Hello {props.name}, you are {props.age} years old</p>
-  </div>
+const Header = p => (
+  <>
+    <h1>{p.course}</h1>
+  </>
+)
+
+const Content = props => (
+  <>
+    <p>
+      {props.part} {props.exe}
+    </p>
+  </>
+)
+
+const Total = props => (
+  <>
+    <p>
+      Number of exercises {props.num}
+    </p>
+  </>
 )
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name='George' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Header course={course} />
+      <Content part={part1} exe={exercises1}/>
+      <Content part={part2} exe={exercises2}/>
+      <Content part={part3} exe={exercises3}/>
+      <Total num={exercises1 + exercises2 + exercises3}/>
     </>
   )
 }
