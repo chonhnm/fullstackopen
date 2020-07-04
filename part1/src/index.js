6dee4c7b435a997f57941680e085568decb6caec
chon <chonhnm@gmail.com>
2020-07-04 16:58:13 +0800

    exe 1.4

--- part1/src/index.js
+++ part1/src/index.js
@@ -36,24 +36,28 @@ const Total = props => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  let total = 0
+  parts.forEach(p => total += p.exercises)
 
   return (
     <>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total num={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total num={total} />
     </>
   )
 }
