2e28d4216423a6a179abc0a083781f53ad2d92d7
chon <chonhnm@gmail.com>
2020-07-04 14:57:20 +0800

    before exercise 1.1

---
+++ part1/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => (
+  <div>
+    <p>Hello {props.name}, you are {props.age} years old</p>
+  </div>
+)
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='George' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
