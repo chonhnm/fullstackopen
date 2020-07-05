0905dcfdeeb65297bf2143dccf4adbcee4105cb9
chon <chonhnm@gmail.com>
2020-07-05 15:10:36 +0800

    exe 1.12

---
+++ part1/unicafe/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const H2 = ({ text }) => <h2>{text}</h2>
+
+const Button = ({ clickHandler, text }) => (
+  <button onClick={clickHandler}>
+    {text}
+  </button>
+)
+
+const Statistic = ({ text, value }) =>
+  (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+
+
+const Statistics = ({ stats, total }) => {
+  if (total === 0) {
+    return (<p>No feedback given</p>)
+  } else {
+    let i = 0
+    return (
+      <>
+        <table>
+          <tbody>
+            {stats.map(stat =>
+              <Statistic text={stat.name} value={stat.value} key={i++} />
+            )}
+          </tbody>
+        </table>
+      </>
+    )
+  }
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const totalClick = bad + neutral + good
+  const average = (good - bad) / totalClick
+  const positive = (good / totalClick * 100) + '%'
+
+  const statistics = [
+    {
+      name: 'good',
+      value: good
+    },
+    {
+      name: 'neutral',
+      value: neutral
+    },
+    {
+      name: 'bad',
+      value: bad
+    },
+    {
+      name: 'all',
+      value: totalClick
+    },
+    {
+      name: 'average',
+      value: average
+    }, {
+      name: 'positive',
+      value: positive
+    }
+  ]
+
+  const goodClickHandler = () => setGood(good + 1)
+  const ntClickHandler = () => setNeutral(neutral + 1)
+  const badClickHandler = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <H2 text='give feedback' />
+      <Button clickHandler={goodClickHandler} text='good' />
+      <Button clickHandler={ntClickHandler} text='neutral' />
+      <Button clickHandler={badClickHandler} text='bad' />
+      <H2 text='statistics' />
+      <Statistics stats={statistics} total={totalClick} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
